846eb8d9fda647dd612dfa4ec66a160e84256714
eldarski <117188568+eldarski@users.noreply.github.com>
2025-11-14 00:52:49 +0500

    Configure for Cloudflare Pages deployment
       - Downgrade Next.js to 15.5.2 and React to 18.3.1
       - Install @cloudflare/next-on-pages adapter
       - Update build script to use next-on-pages
       - Add experimental edge runtime config

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const withNextIntl = createNextIntlPlugin('./i18n.ts');
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    runtime: 'experimental-edge',
+  },
 };
 
 export default withNextIntl(nextConfig);
